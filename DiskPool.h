f9664c2a3bacf5c60f3d52ecd3b4ded68b7fa012
Agustin Gianni <agustingianni@gmail.com>
2016-11-20 18:02:01 +0100

    * Implemented benchmarks using google-benchmark * Implemented single thread and thread safe versions * Removed usage of exceptions

--- DiskPool.h
+++ DiskPool.h
@@ -9,54 +9,115 @@
 #include <string>
 #include <cstddef>
 #include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <cerrno>
+#include <iostream>
 
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
-#include <cerrno>
-#include <stdatomic.h>
 
+#ifdef __APPLE__
+#include <mach/vm_statistics.h>
+#endif
+
+constexpr size_t KB(size_t size) { return size * 1024; }
+constexpr size_t MB(size_t size) { return KB(size) * 1024; }
+constexpr size_t GB(size_t size) { return MB(size) * 1024; }
+
+template<typename T>
 class DiskPool {
+protected:
     uint8_t *m_address;
     size_t m_size;
-    std::atomic<size_t> m_top;
 
 public:
-    DiskPool(const std::string &filename, size_t size) : m_size{size}, m_top{0} {
+    DiskPool(const std::string &filename, size_t size) : m_size{size} {
         auto fd = open(filename.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0644);
         if (fd == -1) {
-            throw std::runtime_error("Failed to open file: " + std::string(strerror(errno)));
+            std::cerr << "Failed to open file: " << strerror(errno) << std::endl;
+            abort();
         }
 
         if (ftruncate(fd, m_size) == -1) {
             close(fd);
-            throw std::runtime_error("Failed to ftruncate file: " + std::string(strerror(errno)));
+            std::cerr << "Failed to ftruncate file: " << strerror(errno) << std::endl;
         }
 
-        m_address = reinterpret_cast<uint8_t *>(mmap(nullptr, m_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+        int flags = MAP_FILE | MAP_SHARED;
+        int prot = PROT_READ | PROT_WRITE;
+
+#ifdef MAP_HUGETLB
+        flags |= MAP_HUGETLB;
+#endif
+
+#ifdef MAP_ALIGNED_SUPER
+        flags |= MAP_ALIGNED_SUPER;
+#endif
+
+#ifdef VM_FLAGS_SUPERPAGE_SIZE_ANY
+        flags |= VM_FLAGS_SUPERPAGE_SIZE_ANY;
+#endif
+
+        m_address = reinterpret_cast<uint8_t *>(mmap(nullptr, m_size, prot, flags, fd, 0));
         if (m_address == MAP_FAILED) {
             close(fd);
-            throw std::runtime_error("Failed to map file: " + std::string(strerror(errno)));
+            std::cerr << "Failed to map file: " << strerror(errno) << std::endl;
+            abort();
         }
 
         if (close(fd) != 0) {
-            throw std::runtime_error("Failed to close file: " + std::string(strerror(errno)));
+            std::cerr << "Failed to close file: " << strerror(errno) << std::endl;
+            abort();
         }
     }
 
     ~DiskPool() {
         if (munmap(m_address, m_size) != 0) {
-            throw std::runtime_error("Failed to unmap file: " + std::string(strerror(errno)));
+            std::cerr << "Failed to unmap file: " << strerror(errno) << std::endl;
+            abort();
         };
     }
 
     uint8_t *alloc(size_t size) {
-        return m_address + m_top.fetch_add(size, std::memory_order_relaxed);
+        return static_cast<T>(this)->alloc(size);
     }
 
     // Flush to disk so we release physical pages.
     void flush() {
-        assert(madvise(m_address, m_size, MADV_DONTNEED) == 0 && "Failed to de-commit memory.");
+        if (madvise(m_address, m_size, MADV_DONTNEED)) {
+            std::cerr << "Failed to flush memory: " << strerror(errno) << std::endl;
+            abort();
+        }
+    }
+};
+
+// Single thread disk pool. This does not implement any locking, use with care.
+class DiskPool_st : public DiskPool<DiskPool_st> {
+    size_t m_top;
+
+public:
+    DiskPool_st(const std::string &filename, size_t size) : DiskPool<DiskPool_st>{filename, size}, m_top{0} {
+    }
+
+    uint8_t *alloc(size_t size) {
+        auto tmp = m_top;
+        m_top += size;
+        return m_address + tmp;
+    }
+};
+
+// Thread safe disk pool. Uses atomic to handle accesses from multiple threads.
+class DiskPool_mt : public DiskPool<DiskPool_mt> {
+    std::atomic<size_t> m_top;
+
+public:
+    DiskPool_mt(const std::string &filename, size_t size) : DiskPool<DiskPool_mt>{filename, size}, m_top{0} {
+    }
+
+    uint8_t *alloc(size_t size) {
+        return m_address + m_top.fetch_add(size, std::memory_order_relaxed);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
-#include <atomic>
 #include <thread>
-#include <chrono>
 #include <iostream>
+
+#include <benchmark/benchmark.h>
 
 #include "DiskPool.h"
 #include "measure.h"
 
-constexpr size_t KB(size_t size) { return size * 1024; }
-constexpr size_t MB(size_t size) { return KB(size) * 1024; }
-constexpr size_t GB(size_t size) { return MB(size) * 1024; }
-
 std::atomic<int> stop{0};
-DiskPool memory_pool("/tmp/memory.log", GB(8));
 
 // Write all the data to disk.
-void io_thread() {
-    using clock = std::chrono::steady_clock;
+void io_thread(DiskPool_mt *memory_pool) {
     using precision = std::chrono::milliseconds;
 
     printf("Starting IO thread.\n");
@@ -23,65 +17,48 @@ void io_thread() {
     while (!stop.load(std::memory_order_acquire)) {
         sleep(1);
 
-        auto t0 = clock::now();
-        memory_pool.flush();
-        auto delta = std::chrono::duration_cast<precision>(clock::now() - t0).count();
+        auto ret = measure<precision>::execution(
+            [memory_pool] {
+                memory_pool->flush();
+                return 0;
+            }
+        );
 
-        printf("Took %llu ms.\n", delta);
+        printf("Took %llu ms.\n", ret.second);
     }
 
     printf("Stopping IO thread.\n");
 }
 
-// Generate all the data.
-void work_thread() {
-    using clock = std::chrono::steady_clock;
-    using precision = std::chrono::nanoseconds;
-
-    printf("Starting Work thread.\n");
-
-    constexpr double alpha = 1.0 / 1000;
-    constexpr double one_minus_alpha = 1.0 - alpha;
-    double accumulator = 0.0;
-
-    for (int i = 0; i < 10000000; i++) {
-        auto t0 = clock::now();
-        auto mem = reinterpret_cast<int *>(memory_pool.alloc(sizeof(i)));
-        auto count = std::chrono::duration_cast<precision>(clock::now() - t0).count();
-
-        // Compute the "average".
-        accumulator = (alpha * count) + one_minus_alpha * accumulator;
-
-        *mem = i;
-    }
-
-    printf("Average time for allocation: %f ns\n", accumulator);
-
-    // Stop the IO thread.
-    stop.store(1, std::memory_order_release);
-
-    printf("Stopping Work thread.\n");
-}
-
-void bench_malloc() {
-    size_t malloc_size = 32;
-    for (auto i = 0; i < 1000; i++) {
-        auto ret = measure<std::chrono::nanoseconds>::execution(malloc, malloc_size);
-        free(ret.first);
-        //printf("Time for malloc(%zu): %lld ns\n", malloc_size, ret.second);
+// Check the single thread performance.
+DiskPool_st memory_pool_st("/tmp/memory.log", GB(8));
+void BM_MemoryAllocation_DiskPool_st(benchmark::State &state, DiskPool_st *pool) {
+    while (state.KeepRunning()) {
+        auto mem = pool->alloc(sizeof(int));
+        benchmark::DoNotOptimize(mem);
     }
 }
 
-int main() {
-    std::thread t1(io_thread);
-    std::thread t2(work_thread);
-    std::thread t3(work_thread);
-    // std::thread t4(bench_malloc);
+BENCHMARK_CAPTURE(BM_MemoryAllocation_DiskPool_st, SingleThreaded, &memory_pool_st);
 
-    t1.join();
-    t2.join();
-    t3.join();
-    //t4.join();
-
-    return 0;
+static DiskPool_mt memory_pool_mt("/tmp/memory.log", GB(8));
+void BM_MemoryAllocation_DiskPool_mt(benchmark::State &state, DiskPool_mt *pool) {
+    DiskPool_st memory_pool("/tmp/memory.log", GB(8));
+    while (state.KeepRunning()) {
+        auto mem = pool->alloc(sizeof(int));
+        benchmark::DoNotOptimize(mem);
+    }
 }
+
+BENCHMARK_CAPTURE(BM_MemoryAllocation_DiskPool_mt, Threaded, &memory_pool_mt)->ThreadRange(1, 32);
+
+void BM_MemoryAllocation_malloc(benchmark::State &state) {
+    while (state.KeepRunning()) {
+        auto mem = malloc(sizeof(int));
+        benchmark::DoNotOptimize(mem);
+    }
+}
+
+BENCHMARK(BM_MemoryAllocation_malloc)->ThreadRange(1, 32);
+
+BENCHMARK_MAIN()
